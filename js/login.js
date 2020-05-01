7678f4a6e4147de95cac01d35137e6787192d075
RPWPA <moataz.alsayed.abdelfatah@gmail.com>
2020-05-01 02:54:50 +0200

    Added alerts

--- js/login.js
+++ js/login.js
@@ -1,3 +1,14 @@
-function login(){
-    alert("Please enter data fully.")
+function login(){    
+        if(document.getElementById("loginError")){
+            if (window.getComputedStyle((document.getElementById("loginError")),null).getPropertyValue("display") == "none") {
+                document.getElementById("loginError").style.display = "block";
+            }
+        }
+}
+function closeLogin(){
+    if(document.getElementById("loginError")){
+        if (window.getComputedStyle((document.getElementById("loginError")),null).getPropertyValue("display") == "block") {
+            document.getElementById("loginError").style.display = "none";
+        }
+    }
 }
--- js/register.js
+++ js/register.js
@@ -1,3 +1,14 @@
 function register(){
-    alert("Please enter data fully.")
+    if(document.getElementById("registerError")){
+        if (window.getComputedStyle((document.getElementById("registerError")),null).getPropertyValue("display") == "none") {
+            document.getElementById("registerError").style.display = "block";
+        }
+    }
+}
+function closeRegister(){
+    if(document.getElementById("registerError")){
+        if (window.getComputedStyle((document.getElementById("registerError")),null).getPropertyValue("display") == "block") {
+            document.getElementById("registerError").style.display = "none";
+        }
+    }
 }
